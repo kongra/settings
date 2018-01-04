26fe271be90262c574a2f2c804bc7b8a0c1b64a7
kongra <kongra@gmail.com>
2018-01-04 21:40:41 +0100

    Moved into Clojure 1.9.0

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,39 +1,37 @@
-{:user
- {:plugins      [[lein-pprint        "1.1.2"]
-                 [lein-ancient      "0.6.10"]
-                 [lein-bikeshed      "0.4.1"]
-                 [jonase/eastwood    "0.2.4"]
-                 [lein-kibit         "0.1.5"
-                  :exclusions
-                  [org.clojure/clojure
-                   org.clojure/tools.namespace
-                   org.clojure/tools.cli]]
+{:user {:plugins [[lein-pprint        "1.2.0"]
+                  [lein-ancient      "0.6.15"]
+                  [lein-bikeshed      "0.5.0"]
+                  [jonase/eastwood    "0.2.5"]
+                  [lein-kibit         "0.1.6"
+                   :exclusions
+                   [org.clojure/tools.namespace
+                    org.clojure/tools.cli]]]
 
-                 [cider/cider-nrepl "0.14.0"]]
-  :signing       {:gpg-key        "CF1B5B02"}
-  :dependencies [[criterium          "0.4.4"]]
+        :signing      {:gpg-key   "CF1B5B02"}
+        :dependencies [[criterium    "0.4.4"]]
 
-  ;; :jvm-opts     ["-server"
-  ;;                "-d64"
-  ;;                "-Dclojure.compiler.direct-linking=true"
+        ;; :jvm-opts     ["-server"
+        ;;                "-d64"
+        ;;                "-Dclojure.compiler.direct-linking=true"
 
-  ;;                "-Xshare:off"
-  ;;                "-XX:+AggressiveOpts"
-  ;;                "-XX:+DoEscapeAnalysis"
-  ;;                "-XX:+UseCompressedOops"
-  ;;                ;; "-XX:+UseNUMA" ;; to check: numactl --hardware
+        ;;                "-Xshare:off"
+        ;;                "-XX:+AggressiveOpts"
+        ;;                "-XX:+DoEscapeAnalysis"
+        ;;                "-XX:+UseCompressedOops"
+        ;;                ;; "-XX:+UseNUMA" ;; to check: numactl --hardware
 
-  ;;                "-Xms1G"
-  ;;                "-Xmx1G"
+        ;;                "-Xms1G"
+        ;;                "-Xmx1G"
 
-  ;;                "-XX:+UseParallelGC"
-  ;;                "-XX:+UseParallelOldGC"
-  ;;                "-XX:NewSize=400m"
-  ;;                "-XX:MaxNewSize=400m"
-  ;;                "-XX:-UseAdaptiveSizePolicy"
-  ;;                "-XX:SurvivorRatio=6"
+        ;;                "-XX:+UseParallelGC"
+        ;;                "-XX:+UseParallelOldGC"
+        ;;                "-XX:NewSize=400m"
+        ;;                "-XX:MaxNewSize=400m"
+        ;;                "-XX:-UseAdaptiveSizePolicy"
+        ;;                "-XX:SurvivorRatio=6"
 
-  ;;                "-XX:+PrintGCDetails"
-  ;;                "-XX:+PrintGCTimeStamps"]
+        ;;                "-XX:+PrintGCDetails"
+        ;;                "-XX:+PrintGCTimeStamps"]
 
-  }}
+        }
+ }
