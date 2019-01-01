ef7e62777846ec984986f8151ed7e0a98cdb0035
Konrad Grzanek <kongra@gmail.com>
2019-01-01 21:29:39 +0100

    Cider 19 added, cleanup.

--- emacs/.emacs.d/elpa/(old) clojure-mode-extra-font-locking-5.7.0/clojure-mode-extra-font-locking-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode-extra-font-locking" "5.7.0" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "e06554845548e911f3cd204f93fdbf2661c6d0a7" :keywords '("languages" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
