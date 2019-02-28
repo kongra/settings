4e0fb9f08c6368fa9327948ff8c8ded68d91f525
Konrad Grzanek <kongra@gmail.com>
2019-02-28 12:32:19 +0100

    Update.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,8 @@
 {:user {:plugins [[lein-pprint        "1.2.0"]
                   [lein-ancient      "0.6.15"]
                   [lein-bikeshed      "0.5.1"]
-                  [jonase/eastwood    "0.3.4"]
-                  [lein-kibit         "0.1.6"
-                   :exclusions
-                   [org.clojure/tools.namespace
-                    org.clojure/tools.cli]]]
+                  [jonase/eastwood    "0.3.5"]
+                  [lein-kibit         "0.1.6" :exclusions [org.clojure/tools.namespace]]]
 
         :signing      {:gpg-key   "CF1B5B02"}
         :dependencies [[criterium    "0.4.4"]]}
