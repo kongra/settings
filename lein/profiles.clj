9c2114d0ddc2d5d1117b3235b3cece84fa497397
kongra <kongra@gmail.com>
2019-10-17 20:42:58 +0200

    clj-kondo added

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,10 @@
 {:user {:plugins [[lein-pprint        "1.2.0"]
                   [lein-ancient      "0.6.15"]
-                  [lein-bikeshed      "0.5.1"]
-                  [jonase/eastwood    "0.3.5"]
+                  [lein-bikeshed      "0.5.2"]
+                  [jonase/eastwood    "0.3.6"]
                   [lein-kibit         "0.1.6" :exclusions [org.clojure/tools.namespace]]]
 
         :signing      {:gpg-key   "CF1B5B02"}
-        :dependencies [[criterium    "0.4.4"]]}
- }
+        :dependencies [[criterium    "0.4.5"]
+                       [clj-kondo  "RELEASE"]]
+        :aliases      {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}}
