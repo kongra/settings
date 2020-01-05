ce46038e82762281ba4fb31e4d0808b1f8f0bf6b
kongra <kongra@gmail.com>
2020-01-05 10:00:47 +0100

    Visual update.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -173,7 +173,7 @@
 
 (setq display-line-numbers-type 'relative)
 (set-face-background 'line-number              "#292b2e")
-(set-face-background 'line-number-current-line "#242629")
+(set-face-background 'line-number-current-line "#222226")
 
 ;; TRANSIENT-MARK MODE
 (transient-mark-mode 1)
@@ -561,6 +561,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
     (color-moccur spacemacs-theme which-key use-package))))
