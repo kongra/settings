8a1a4d2f5ba0b0435e303ed4eff8c0d4f91d7bab
kongra <kongra@gmail.com>
2020-03-13 08:02:30 +0100

    Update.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[lein-pprint        "1.2.0"]
+{:user {:plugins [[lein-pprint        "1.3.2"]
                   [lein-ancient      "0.6.15"]
                   [lein-bikeshed      "0.5.2"]
-                  [jonase/eastwood    "0.3.6"]
-                  [lein-kibit         "0.1.6" :exclusions [org.clojure/tools.namespace]]]
+                  [jonase/eastwood   "0.3.10"]
+                  [lein-kibit         "0.1.8" :exclusions [org.clojure/tools.namespace]]]
 
         :signing      {:gpg-key   "CF1B5B02"}
         :dependencies [[criterium    "0.4.5"]
